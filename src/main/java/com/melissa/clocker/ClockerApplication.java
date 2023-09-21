e6deaca8217f5a62798dd44c2be60ef3fd6e4f66
melissa <melissastan91@gmail.com>
2023-09-21 07:39:39 -0400

    initialize the repo

---
+++ src/main/java/com/melissa/clocker/ClockerApplication.java
@@ -0,0 +1,13 @@
+package com.melissa.clocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClockerApplication.class, args);
+	}
+
+}
